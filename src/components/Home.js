3ca8588e98dd16e7b8e60454ca33a9401cb6534f
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2023-02-24 22:26:00 +0100

    Removed unecessary setBlog function

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
     // custom hook
 
-    const { data:blogs, setData:setBlogs ,isPending, error} = useFetch('http://localhost:8000/blogs');
+    const { data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
     
     return ( 
         <div className="home">
